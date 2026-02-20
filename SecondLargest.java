79f8720dd8d6ec2817913ea7802f871260e8db12
rajeshkumarbehera261 <rajeshkumarbehera261@gmail.com>
2026-02-20 20:14:47 +0530

    Second Largest Program is added

--- SecondLargest.java
+++ SecondLargest.java
@@ -7,7 +7,7 @@ public class SecondLargest {
         for(int i =0; i<arr.length; i++){
             if(arr[i]>largest){
                 secondLargest = largest;
-                largest = arr[i];
+                largest = arr [i];
             }else if(arr[i]>secondLargest && arr[i] != largest){
                 secondLargest = arr[i];
             }
